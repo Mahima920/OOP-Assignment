ab941e46f34042b26c2d26bad16282382608f4b5
Mahima920 <mahimaadhikari456@gmail.com>
2025-02-22 22:42:08 +0545

    Loops numeric

---
+++ qns2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Input a number to find the last prime number occurs before the number: ";
+    cin >> n;
+    for(int i = n - 1; i > 1; i--){
+        bool isPrime = true;
+        for(int j = 2; j * j <= i; j++){
+            if(i % j == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            cout << i << " is the last prime number before " << n << endl;
+            break;
+        }
+    }
+    return 0;
+}
---
+++ qns3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Input number of terms: ";
+    cin >> n;
+    int sum = 0;
+    int term = 0;
+    for(int i = 0; i < n; i++){
+        term = term * 10 + 1;
+        sum += term;
+        cout << term;
+        if(i < n - 1){
+            cout << " + ";
+        }
+    }
+    cout << endl << "The sum of the series is: " << sum << endl;
+    return 0;
+}
