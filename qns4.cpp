581a53ff2376a0bb95814ac0e1da089a0817b72d
Mahima920 <mahimaadhikari456@gmail.com>
2025-02-22 22:36:37 +0545

    Recursion concept
    Recursion

---
+++ qns4.cpp
@@ -0,0 +1,17 @@
+// Write a C++ program to reverse a string using recursion.
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverseString(const string &s) {
+    if (s.empty()) return s;
+    return reverseString(s.substr(1)) + s[0];
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "The reversed string: " << reverseString(str) << endl;
+    return 0;
+} 
---
+++ qns7.cpp
@@ -0,0 +1,27 @@
+// Write a C++ program to implement a recursive function to find the sum of all prime numbers in a given range.
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num, int i = 2) {
+    if (num < 2) return false;
+    if (i * i > num) return true;
+    if (num % i == 0) return false;
+    return isPrime(num, i + 1);
+}
+
+int sumPrimes(int start, int end) {
+    if (start > end) return 0;
+    return (isPrime(start) ? start : 0) + sumPrimes(start + 1, end);
+}
+
+int main()
+ {
+    int s, e;
+    cout << "Enter start of the range: ";
+    cin >> s;
+    cout << "Enter end of the range: ";
+    cin >> e;
+
+    cout << "The sum of prime numbers in the range: " << sumPrimes(s, e) << endl;
+    return 0;
+}
