6810be0c26ff48097abd097bfaa7606527a91243
Mahima920 <mahimaadhikari456@gmail.com>
2025-02-22 22:46:28 +0545

    Array

---
+++ qns1.cpp
@@ -0,0 +1,46 @@
+// Write a C++ program to find the largest three elements in an array.
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void findLargestThree(int arr[], int size) {
+    if (size < 3) {
+        cout << "The array should have at least 3 elements." << endl;
+        return;
+    }
+    int first = INT_MIN, second = INT_MIN, third = INT_MIN;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > first) {
+            third = second;
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second) {
+            third = second;
+            second = arr[i];
+        } else if (arr[i] > third) {
+            third = arr[i];
+        }
+    }
+
+    cout << "The largest three elements are: ";
+    cout << first << " " << second << " " << third << endl;
+}
+
+int main() {
+    int size;
+
+    cout << "Please enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Please enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    findLargestThree(arr, size);
+
+    return 0;
+}
